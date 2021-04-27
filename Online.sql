b9e9ac0d8c3a5f35a6e85805e690cc5839cbe9df
San Kim <kim503@usc.edu>
2021-04-27 16:32:25 -0700

    Update Online.sql

--- Online.sql
+++ Online.sql
@@ -5,3 +5,10 @@ CREATE TABLE IF NOT EXISTS Queue (
     park varchar(255),
     location varchar(255)
 );
+
+CREATE TABLE IF NOT EXISTS UserTable (
+	userID int,
+    firstname varchar(255),
+    lastname varchar(255),
+    userpassword varchar(255)
+);
